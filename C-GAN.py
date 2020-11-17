5ba339abb6da7b559b71a5e946047565b5a821dd
letiziaia <letizia852@gmail.com>
2020-11-17 23:49:10 +0200

    improved cgan, saving synthetic ts

--- C-GAN.py
+++ C-GAN.py
@@ -12,16 +12,18 @@ from DataLoader import DataLoader
 
 
 class CGAN():
-    def __init__(self, input_shape, nb_classes, latent_dim=100, epochs=10000, mini_batch_size=15):
+    def __init__(self, input_shape, nb_classes, latent_dim=600, epochs=10000, mini_batch_size=15,
+                 n_synthetic_per_class=10):
         # Input shape
-        self.img_rows = input_shape[0]
-        self.img_cols = input_shape[1]
-        self.img_shape = (self.img_rows, self.img_cols)
+        self.input_rows = input_shape[0]
+        self.input_cols = input_shape[1]
+        self.input_shape = (self.input_rows, self.input_cols)
         self.num_classes = nb_classes
         self.latent_dim = latent_dim
         self.mini_batch_size = mini_batch_size
         self.nb_classes = nb_classes
         self.epochs = epochs
+        self.n_synthetic = n_synthetic_per_class
 
         optimizer = Adam(0.0002, 0.5)
 
@@ -66,8 +68,8 @@ class CGAN():
         model.add(Dense(1024))
         model.add(LeakyReLU(alpha=0.2))
         model.add(BatchNormalization(momentum=0.8))
-        model.add(Dense(np.prod(self.img_shape), activation='tanh'))
-        model.add(Reshape(self.img_shape))
+        model.add(Dense(np.prod(self.input_shape), activation='tanh'))
+        model.add(Reshape(self.input_shape))
 
         model.summary()
 
@@ -83,7 +85,7 @@ class CGAN():
     def build_discriminator(self):
 
         model = Sequential()
-        model.add(Dense(512, input_dim=np.prod(self.img_shape)))
+        model.add(Dense(512, input_dim=np.prod(self.input_shape)))
         model.add(LeakyReLU(alpha=0.2))
         model.add(Dense(512))
         model.add(LeakyReLU(alpha=0.2))
@@ -94,10 +96,10 @@ class CGAN():
         model.add(Dense(1, activation='sigmoid'))
         model.summary()
 
-        img = Input(shape=self.img_shape)
+        img = Input(shape=self.input_shape)
         label = Input(shape=(1,), dtype='int32')
 
-        label_embedding = Flatten()(Embedding(self.num_classes, np.prod(self.img_shape))(label))
+        label_embedding = Flatten()(Embedding(self.num_classes, np.prod(self.input_shape))(label))
         flat_img = Flatten()(img)
 
         model_input = multiply([flat_img, label_embedding])
@@ -106,12 +108,13 @@ class CGAN():
 
         return Model([img, label], validity)
 
-    def train(self, X_train, y_train, sample_interval=100):
+    def train_generate(self, X_train, y_train, checkpoint_interval=1000):
 
         print("classes: ", self.nb_classes)
         for i in range(self.nb_classes):
+            # get the first sample in class i from the original set
             class_i = np.where(y_train == i)[0][0]
-            print(class_i)
+            print("Plot original sample n. ", class_i)
             data = np.ravel(X_train[class_i, :, :])
             sns.lineplot(x=range(X_train.shape[1]), y=data, label=f"orig {i}")
             plt.show()
@@ -126,14 +129,14 @@ class CGAN():
             #  Train Discriminator
             # ---------------------
 
-            # Select a random half batch of images
+            # Select a random batch of images
             idx = np.random.randint(0, X_train.shape[0], self.mini_batch_size)
             imgs, labels = X_train[idx], y_train[idx]
 
             # Sample noise as generator input
-            noise = np.random.normal(0, 1, (self.mini_batch_size, 100))
+            noise = np.random.normal(0, 1, (self.mini_batch_size, self.latent_dim))
 
-            # Generate a half batch of new images
+            # Generate a batch of new samples
             gen_imgs = self.generator.predict([noise, labels])
 
             # Train the discriminator
@@ -146,7 +149,7 @@ class CGAN():
             # ---------------------
 
             # Condition on labels
-            sampled_labels = np.random.randint(0, 2, self.mini_batch_size).reshape(-1, 1)
+            sampled_labels = np.random.randint(0, self.nb_classes, self.mini_batch_size).reshape(-1, 1)
 
             # Train the generator
             g_loss = self.combined.train_on_batch([noise, sampled_labels], valid)
@@ -154,17 +157,20 @@ class CGAN():
             # Plot the progress
             print("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100 * d_loss[1], g_loss))
 
-            # If at save interval => save generated image samples
-            if epoch % sample_interval == 0:
+            # Every checkpoin_interval epochs, view the generated samples
+            if epoch % checkpoint_interval == 0:
                 self.sample_images()
         self.sample_images()
+        ts, l = self._generate()
+        return ts, l
 
     def sample_images(self):
-        noise = np.random.normal(0, 1, (self.nb_classes, 100))
+        noise = np.random.normal(0, 1, (self.nb_classes, self.latent_dim))
         sampled_labels = np.arange(0, self.nb_classes).reshape(-1, 1)
 
         gen_imgs = self.generator.predict([noise, sampled_labels])
 
+        print("Shape of generated data:")
         print(gen_imgs.shape)
         print(sampled_labels)
         for i in range(gen_imgs.shape[0]):
@@ -172,12 +178,29 @@ class CGAN():
             sns.lineplot(x=range(gen_imgs.shape[1]), y=np.ravel(gen_imgs[i, :, :]), label=f"gen {label}")
             plt.show()
 
+    def _generate(self):
+        new_samples = []
+        new_labels = []
+        # Each generation steps creates one sample per class
+        for i in range(self.n_synthetic):
+            noise = np.random.normal(0, 1, (self.nb_classes, self.latent_dim))
+            sampled_labels = np.arange(0, self.nb_classes).reshape(-1, 1)
+
+            gen_imgs = self.generator.predict([noise, sampled_labels])
+            for i in range(gen_imgs.shape[0]):
+                new_ts = np.ravel(gen_imgs[i, :, :])
+                new_samples.append(new_ts)
+                label = sampled_labels[i]
+                new_labels.append(label)
+        return np.array(new_samples), np.array(new_labels)
+
 
 if __name__ == '__main__':
-    dt = DataLoader(path="C:/Users/letiz/Desktop/Bachelor\'s Thesis and Seminar - JOIN.bsc/data")
+    dt = DataLoader(path="C:/Users/letiz/Desktop/Bachelor\'s Thesis and Seminar - JOIN.bsc/data", data_name='insect')
     X_train, y_train, _, _ = dt.get_X_y(one_hot_encoding=False)
     nb_classes = len(np.unique(y_train))
 
+    print(X_train.shape)
     # Scale input
     scaler = MinMaxScaler()
     X_train = scaler.fit_transform(X_train)
@@ -190,9 +213,16 @@ if __name__ == '__main__':
     input_shape = X_train.shape[1:]
     print(X_train.shape)
 
-    y_train = np.array([0 if x == 1 else 1 for x in y_train])
+    y_train = np.array([x - 1 for x in y_train])
     y_train = y_train.reshape(-1, 1)
     print(y_train.shape)
 
-    cgan = CGAN(input_shape=input_shape, nb_classes=nb_classes)
-    cgan.train(X_train, y_train, sample_interval=1000)
+    cgan = CGAN(input_shape=input_shape, nb_classes=nb_classes,
+                mini_batch_size=input_shape[0],
+                latent_dim=input_shape[1], epochs=1000, n_synthetic_per_class=3)
+    ts, l = cgan.train_generate(X_train, y_train, checkpoint_interval=10000)
+    # ts = scaler.inverse_transform(ts)
+    # for i in range(min(ts.shape[0], 3)):
+    #     label = l[i]
+    #     sns.lineplot(x=range(ts.shape[1]), y=np.ravel(ts[i, :]), label=f"gen {label}")
+    #     plt.show()
